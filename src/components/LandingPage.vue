8c7f7ec7bb9c6cf8faddb508160ef39e99a062eb
Will Harris <will.harris@valtech.com>
2019-10-26 21:16:42 +0100

    Vuetify set up correctly

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,24 @@
+<template>
+<body>
+    <div id="background">
+    hello
+    </div>
+
+<link rel="stylesheet" type="text/css" href="landingpage.css">
+</body>
+</template>
+
+<script>
+import '../css/landingpage.css'
+
+
+export default {
+    name: 'LandingPage'
+}
+</script>
+
+
+<style>
+
+</style>
+
--- src/main.js
+++ src/main.js
@@ -2,17 +2,36 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css';
-
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+import VueRouter from 'vue-router'
+import LandingPage from './components/LandingPage.vue'
 
 Vue.use(Vuetify)
 
 const opts = {}
 
 export default new Vuetify(opts)
+
+const routes = [
+  { path: '/', component: LandingPage }
+]
+
+const router = new VueRouter({
+  routes
+});
+
+const vuetify = new Vuetify()
+
+Vue.use(VueRouter)
+
+new Vue({
+  vuetify,
+  router,
+  el: '#app',
+  render: h => h(App),
+  components: {
+    App
+  }
+})
 
 
 
